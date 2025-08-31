90526accf631d2cb1c163e39f229d9760604ce92
potatopepsi <137965266+potatopepsi@users.noreply.github.com>
2025-08-31 03:49:50 -0400

    Added credits

--- js/index.js
+++ js/index.js
@@ -1,41 +1,48 @@
-// /*
-// How to make a growing span
-// https://dev.to/matrixersp/how-to-make-an-input-field-grow-shrink-as-you-type-513l
-// */ 
-
+// Declaring const values
 const input = document.querySelector('input');
-// const span = document.querySelector('span');
-
-
-// input.addEventListener('input', function (event) {
-//     span.innerHTML = this.value.replace(/\s/g, '&nbsp;');
-//     this.style.width = span.offsetWidth + 'px';
-// });
-
-/* End of Code used
- */
-
+const GRABDIV = document.getElementById("display");
 
 // Submitting form on Enter key
-
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter"){
         event.preventDefault();
         document.getElementById("submitting").click();
     }
-    
-
 });
-
+var firstHoldTemp = [];
+//Auto fill for Tab, and Up/Down Functionality
 input.addEventListener("keydown", function(event) {
     if (event.key === "Tab"){
         event.preventDefault();
         autofill();
     }
+    if (event.key == "ArrowUp"){
+        event.preventDefault();
+        firstHoldTemp.push(document.getElementById("command").value);
+        if(counter <= (History.length+1) && counter > 0){
+            counter = counter - 1;
+            getMyHistory(counter);
+        }
+           
+    }else if(event.key == "ArrowDown"){
+        event.preventDefault();
+        firstHoldTemp.push(document.getElementById("command").value);
+        if(counter != (History.length-1)){
+            counter = counter + 1;
+            getMyHistory(counter);
+        }else{
+            document.getElementById("command").value = firstHoldTemp[0];
+        }
+    }else{
+        firstHoldTemp = [];
+    }
+
 });
 
-// Declaring const values
-const GRABDIV = document.getElementById("display");
+var History = [];
+function getMyHistory(counter){
+    document.getElementById("command").value = History[counter];
+}
 
 function sendHelp(){
     const help = [
@@ -58,7 +65,6 @@ function sendHelp(){
     
 }
 
-
 function sendWho(){
     const whoAmI = [
         "",
@@ -80,12 +86,12 @@ function sendContact(){
 
     newLine();
 
-    const githubB = document.createTextNode("Coding"+Array(39).fill('\xa0').join(''));
+    const githubB = document.createTextNode("GitHub"+Array(39).fill('\xa0').join(''));
     GRABDIV.appendChild(githubB);
 
 
     var a = document.createElement('a');
-    var linkText = document.createTextNode("GITHUB");
+    var linkText = document.createTextNode("github.com/potatopepsi");
     a.appendChild(linkText);
     a.title = "github.com/potatopepsi";
     a.target = "_blank";
@@ -94,11 +100,11 @@ function sendContact(){
 
     newLine();
 
-    const linkedinB = document.createTextNode("Job Social"+Array(35).fill('\xa0').join(''));
+    const linkedinB = document.createTextNode("LinkedIn"+Array(37).fill('\xa0').join(''));
     GRABDIV.appendChild(linkedinB);
 
     b = document.createElement('a');
-    linkText = document.createTextNode("LINKEDIN");
+    linkText = document.createTextNode("linkedin.com/in/catherinequachx");
     b.appendChild(linkText);
     b.title = "linkedin.com/in/catherinequachx/";
     b.target = "_blank";
@@ -111,7 +117,7 @@ function sendContact(){
     GRABDIV.appendChild(emailB);
 
     c = document.createElement('a');
-    linkText = document.createTextNode("EMAIL");
+    linkText = document.createTextNode("catherinequachx@gmail.com");
     c.appendChild(linkText);
     c.title = "mailto:catherinequachx@gmail.com";
     c.href="mailto:catherinequachx@gmail.com";
@@ -119,20 +125,22 @@ function sendContact(){
 
     newLine();
     
+    newLine();
 
 }
 
 function sendExperience(){
     const experience = [
-        "Software Engineer - AI Consultant"+Array(40).fill('\xa0').join('')+"Feb 2024 - Oct 2024",
+        "",
+        "Software Engineer - AI Consultant"+Array(24).fill('\xa0').join('')+"Feb 2024 - Oct 2024",
         "Outlier AI - Part-Time",
         "",
-        "Web Developer Intern"+Array(60).fill('\xa0').join('')+"May 2023 - Oct 2023",
+        "Web Developer Intern"+Array(37).fill('\xa0').join('')+"May 2023 - Oct 2023",
         "Accutech Machine, Inc. - Internship",
         "",
-        "Software Engineer Intern"+Array(55).fill('\xa0').join('')+"Jun 2019 - Aug 2020",
+        "Software Engineer Intern"+Array(33).fill('\xa0').join('')+"Jun 2019 - Aug 2020",
         "MK Services Inc. - Internship",
-        
+        ""
     ];
     
 
@@ -145,42 +153,61 @@ function sendExperience(){
 
 function sendProject(){
     const project = [
-        "Timer"+Array(120).fill('\xa0').join('')+"Aug 2025 - Present",
-        "-Developing a Timer app for Android phones with custom themes",
-        "Language used: Kotlin",
         "",
-        "Through the Universe"+Array(94).fill('\xa0').join('')+"Jan 2025 - Feb 2025",
-        "- Developed a short walking story game in Godot Engine",
-        "Language used: GDScript",
+        "Vending Machine Simulator"+Array(90).fill('\xa0').join('')+"Jun 2022 - Jul 2022",
         "",
-        "Discord Bot"+Array(109).fill('\xa0').join('')+"Dec 2023 - Feb 2024",
-        "Developed a bot written in Python to automate an RPG with a database",
-        "to store up to 100 different users’ statistics, up-to-date currency,",
-        "and class information",
-        "Language used: Python",
+        "- Drafted the system using use case diagrams, sequence diagrams, sequence architecture diagrams,",
+        "class diagrams, state-machine diagrams, and ER diagrams.",
         "",
-        "Client's Website for Personal Business"+Array(67).fill('\xa0').join('')+"Feb 2023 - Mar 2023",
-        "- Designed and developed a visually appealing portfolio website using HTML, CSS, and JavaScript.",
-        "- Collaborated with the artist to gather requirements, provide design recommendations, and deliver",
-        " a website that effectively showcases their artwork.",
-        "Languages used: HTML, CSS, JavaScript",
         "",
-        "C2 Framework"+Array(105).fill('\xa0').join('')+"Oct 2022 - Dec 2022",
-        "- Built a C2 framework in React that could store up to 10 different implants and it spoke directly",
-        "to an implant on a victim machine.",
-        "- Designed an implant in C++ could make remote procedure calls, steal data from Chrome, execute",
-        "commands on the terminal remotely, and encrypt data on the victim machine.",
-        "- Maintained proper documentation and reduced clutter in code by overwriting bad coding practices.",
-        "Languages used: React, Windows API, C",
+
+        "Photo Album Web Application"+Array(88).fill('\xa0').join('')+"Jun 2022 - Jul 2022",
         "",
-        "Photo Album Web Application"+Array(80).fill('\xa0').join('')+"Jun 2022 - Jul 2022",
         "- Implemented an SQL database for 10s of users to upload into the system via HTML.",
         "- Generated a space for users to view galleries of photos and make user to user interaction.",
         "Languages used: HTML, CSS, SQL",
         "",
-        "Vending Machine Simulator"+Array(85).fill('\xa0').join('')+"Jun 2022 - Jul 2022",
-        "- Drafted the system using use case diagrams, sequence diagrams, sequence architecture diagrams,",
-        "class diagrams, state-machine diagrams, and er diagrams.",
+        "",
+        "C2 Framework"+Array(103).fill('\xa0').join('')+"Oct 2022 - Dec 2022",
+        "",
+        "- Built a C2 framework in React that could store up to 10 different implants and it spoke directly",
+        "to an implant on a victim machine.",
+        "- Designed an implant in C++ that could make remote procedure calls, steal data from Chrome, execute",
+        "commands on the terminal remotely, and encrypt data on the victim machine.",
+        "- Maintained proper documentation and reduced clutter in code by overwriting bad coding practices.",
+        "Languages/Libraries used: React, Windows API, C",
+        "",
+        "",
+        "Client's Website for Personal Business"+Array(77).fill('\xa0').join('')+"Feb 2023 - Mar 2023",
+        "",
+        "- Designed and developed a visually appealing portfolio website.",
+        "- Collaborated with the artist to gather requirements, provide design recommendations, and deliver",
+        " a website that effectively showcases their artwork.",
+        "Languages used: HTML, CSS, JavaScript",
+        "",
+        "",
+        "Discord Bot"+Array(104).fill('\xa0').join('')+"Dec 2023 - Feb 2024",
+        "",
+        "Developed a bot written in Python to automate an RPG with a database, to store up to 100 different",
+        "users’ statistics, up-to-date currency, and class information.",
+        "Language used: Python",
+        "",
+        "",
+        "Through the Universe"+Array(95).fill('\xa0').join('')+"Jan 2025 - Feb 2025",
+        "",
+        "- Developed a short walking story game in Godot Engine",
+        "Language used: GDScript",
+        "",
+        "",
+      
+        
+        //         "Timer"+Array(110).fill('\xa0').join('')+"Aug 2025 - Present",
+        // "",
+        // "-Developing a Timer app for Android phones with custom themes",
+        // "Language used: Kotlin",
+        // "",
+        // ""
+        
 
     ];
 
@@ -193,13 +220,13 @@ function sendProject(){
 
 function sendEducation(){
     const education = [
-        "Boston University"+Array(60).fill('\xa0').join('')+"May 2023",
+        "",
+        "Boston University"+Array(40).fill('\xa0').join('')+"Sept 2019 - May 2023",
         "Bachelor of Arts - BA, Computer Science",
         "",
-        "Yonsei University"+Array(60).fill('\xa0').join('')+"Jun 2021 - Aug 2021",
+        "Yonsei University"+Array(40).fill('\xa0').join('')+"Jun 2021 - Aug 2021",
         "International Summer Study Abroad Program",
-        "",
-        "Masconomet Regional High School"+Array(32).fill('\xa0').join('')+"Jun 2018",
+        ""
         
     ];
     
@@ -213,13 +240,15 @@ function sendEducation(){
 
 function sendList(){
     const languages=[
-        "C",
-        "C++",
-        "Java",
+        "",
         "Python",
+        "Java",
         "HTML/CSS",
         "Javascript",
-        "Kotlin"
+        "C",
+        "C++",
+        "Kotlin",
+        ""
     ];
     for(let i = 0 ; i < languages.length ; i++){
         const helpText = document.createTextNode(languages[i]);
@@ -262,14 +291,16 @@ function autofill(){
     
 }
 
-
+var counter = 0;
 function commandProc(){
     var input = document.getElementById("command").value;
     const historyText = document.createTextNode("C : \ > " + String(input));
     GRABDIV.appendChild(historyText);
     newLine();
     
-
+    History.push(String(input));
+    counter = History.length ;
+    
     switch(String(input)){
         case "help":
             sendHelp();
@@ -309,5 +340,7 @@ function commandProc(){
             break;
 
     }
+    // https://stackoverflow.com/questions/11715646/scroll-automatically-to-the-bottom-of-the-page
+    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
 }
 
